4c9a698639f621960acd8703f9b4d0df428a5dce
Saransh Parashar <spsaransh.parashar@gmail.com>
2019-04-28 01:10:44 +0530

    Register user added .

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using DatingApp.API.Dtos;
+using DatingApp.API.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly IAuthRepository _repo;
+
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody]UserForRegisterDto userForRegisterDto)
+        {
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+            if (await _repo.UserExists(userForRegisterDto.Username))
+            {
+                return BadRequest();
+            }
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+            return StatusCode(201);
+        }
+    }
+
+ 
+}
---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        public String Username { get; set; }
+        public String Password { get; set; }
+    }
+}
